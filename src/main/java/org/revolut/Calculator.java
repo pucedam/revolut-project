38be080eded348a64b92903b2e99083f0ff9be60
Pedro Uceda <pedro.uceda@clarity.ai>
2025-01-29 16:01:50 +0100

    Add logs

--- src/main/java/org/revolut/Calculator.java
+++ src/main/java/org/revolut/Calculator.java
@@ -1,6 +1,11 @@
 package org.revolut;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculator {
+
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
   private final CalculatorDependency dependency;
 
@@ -9,14 +14,17 @@ public class Calculator {
   }
 
   public int add(int a, int b) {
+    logger.info("Add");
     return a + b;
   }
 
   public int subtract(int a, int b) {
+    logger.info("Subtract");
     return a - b;
   }
 
   public int multiplyUsingDependency(int a, int b) {
+    logger.info("Multiply");
     return dependency.multiply(a, b);
   }
 }
